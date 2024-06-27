2bdd3f2604876098377b3e2ebdfeabd864ee7f6e
John Doe <johndoe@example.com>
2024-06-27 21:40:39 +0300

    le vanilla added

---
+++ levanilla/js/main.js
@@ -0,0 +1,18 @@
+const breakPoint = 900;
+let mainMenu, burgerIcon;
+window.addEventListener('load', () => {
+    const mainMenu = document.querySelector("header nav.main-menu");
+    const burgerIcon = document.querySelector("header nav#burger-icon");
+    if(!burgerIcon) return;
+    burgerIcon.addEventListener("click", () => {
+        mainMenu.classList.toggle("block");
+        burgerIcon.textContent = (burgerIcon.textContent === "⛌") ? "☰" : "⛌";
+    });
+
+    window.addEventListener("resize", () => {
+        if(window.innerWidth > breakPoint) {
+            mainMenu.classList.remove("block");
+            burgerIcon.textContent = "☰";
+        }
+    });
+});
