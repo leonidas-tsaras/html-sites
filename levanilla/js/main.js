108fbff97c867324a7e19588f21e64d87b86c7b5
leonidas-tsaras <leonidas.tsaras@gmail.com>
2024-11-30 12:56:57 +0200

    levanilla

--- levanilla/js/main.js
+++ levanilla/js/main.js
@@ -1,18 +1,20 @@
 const breakPoint = 900;
 let mainMenu, burgerIcon;
 window.addEventListener('load', () => {
-    const mainMenu = document.querySelector("header nav.main-menu");
-    const burgerIcon = document.querySelector("header nav#burger-icon");
-    if(!burgerIcon) return;
-    burgerIcon.addEventListener("click", () => {
-        mainMenu.classList.toggle("block");
-        burgerIcon.textContent = (burgerIcon.textContent === "⛌") ? "☰" : "⛌";
-    });
+  const mainMenu = document.querySelector("header nav.main-menu");
+  const burgerIcon = document.querySelector("header nav#burger-icon");
+  if (!burgerIcon) return;
+  burgerIcon.addEventListener("click", () => {
+    mainMenu.classList.toggle("block");
+    burgerIcon.textContent = (burgerIcon.textContent === "⛌") ? "☰" : "⛌";
+  });
 
-    window.addEventListener("resize", () => {
-        if(window.innerWidth > breakPoint) {
-            mainMenu.classList.remove("block");
-            burgerIcon.textContent = "☰";
-        }
-    });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > breakPoint) {
+      mainMenu.classList.remove("block");
+      burgerIcon.textContent = "☰";
+    }
+  });
+
 });
+
